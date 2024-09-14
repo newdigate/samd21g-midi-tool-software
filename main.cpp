da9e98456277e88d1b10997f9fda91ee42a1e149
newdigate <nicnewdigate@gmail.com>
2024-09-14 16:15:22 +0100

    menu improvements

--- main.cpp
+++ main.cpp
@@ -24,17 +24,27 @@ st7735_opengl<Encoder,Button> tft = st7735_opengl<Encoder,Button>(false, 0, &enc
 VirtualView mainView(tft, 0,0, 128, 128);
 
 TeensyMenu *mainMenu = new TeensyMenu(mainView, 128, 64, 0, 0, RGB565_Sapphire, 0);
-TeensyMenuItem dialogMenuItems[6] = {
+TeensyMenuItem dialogMenuItems[] = {
     TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Settings", 0, 0);}, 8),
     TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Theory", 0, 0);}, 8),
-    TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Practise", 0, 0);}, 8),
     TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Play", 0, 0);}, 8),
     TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Record", 0, 0);}, 8),
     TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Loop", 0, 0);}, 8),
+    TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Spy", 0, 0);}, 8),
+    TeensyMenuItem(*mainMenu, [] (View *v) {v->drawString("Sleep", 0, 0);}, 8),
   };
 
 SceneController controller(mainView, encoderUpDown, encoderLeftRight, button, button2,button3);
-
+const char* menuDescriptions[] = {
+    "Change device settings",
+    "Interactive and non-interactive lessons",
+    "Play songs at your own pace, or any other pace",
+    "Play songs in a non-interactive way",
+    "Record your song",
+    "Looping recorder",
+    "Silent record",
+    "Sleep","h","i","j","k","m","n","o","p"};
+int lastMenuItemIndex = 0;
 Scene *settingsScene = new Scene(
                         _bmp_settings_on,
                         _bmp_settings_off,
@@ -45,19 +55,31 @@ Scene *settingsScene = new Scene(
                         [] {
                             mainView.fillScreen(RGB565_Blue);
                             mainMenu->NeedsUpdate = true;
+                            mainView.setTextColor(RGB565_White);
+                            mainView.setTextWrap(true);
+                            mainView.drawString(menuDescriptions[mainMenu->GetSelectedIndex()], 0,64);
                         },   //             std::function<void()> initScreen = nullptr,
-                        [] { },   //             std::function<void()> uninitScreen = nullptr,
+                        [] {
+
+                        },   //             std::function<void()> uninitScreen = nullptr,
                         [] (unsigned index) {
                             mainMenu->ButtonDown(index);
                         }, //             std::function<void(unsigned)> buttonPressed = nullptr,
                         [] (bool forward) {
                             if (forward) mainMenu->IncreaseSelectedIndex();
                             else mainMenu->DecreaseSelectedIndex();
+                            mainView.setTextWrap(true);
+                            if (lastMenuItemIndex != -1) {
+                                mainView.setTextColor(RGB565_Blue);
+                                mainView.drawString(menuDescriptions[lastMenuItemIndex], 0,64);
+                            }
+                            mainView.setTextColor(RGB565_White);
+                            mainView.drawString(menuDescriptions[mainMenu->GetSelectedIndex()], 0,64);
+                            lastMenuItemIndex = mainMenu->GetSelectedIndex();
                         }, //std::function<void(bool)> rotary1Changed = nullptr,
                         [] (bool forward) { } //std::function<void(bool)> rotary2Changed = nullptr
                         );
 
-std::string menuDescriptions[] = { "Change device settings",""};
 
 void setup() {
 
