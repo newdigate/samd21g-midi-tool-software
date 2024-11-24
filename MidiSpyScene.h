d6aef64ea612ee1d627837374bb2375c9a3a1572
newdigate <nicnewdigate@gmail.com>
2024-11-24 10:14:56 +0000

    media position, MediaButtonBarMenuItem

--- MidiSpyScene.h
+++ MidiSpyScene.h
@@ -12,11 +12,34 @@
 #include "smfwriter.h"
 #include "seven_segment.h"
 
+class MediaPosition {
+public:
+    [[nodiscard]] unsigned GetMilliseconds() const {
+        return _milliseconds;
+    }
+
+    void SetMilliseconds(unsigned milliseconds) {
+        _milliseconds = milliseconds;
+    }
+
+protected:
+    unsigned _milliseconds = 0;
+};
+
+enum MediaButtonType {
+    None,
+    Stop,
+    Play,
+    Pause,
+    Record,
+    Fastfwd
+};
 class MediaButtonBarMenuItem : public TeensyMenuItem {
 public:
-    explicit MediaButtonBarMenuItem(TeensyMenu &menu) :
+    explicit MediaButtonBarMenuItem(TeensyMenu &menu, std::function<void(MediaButtonType)> buttonPressedCallback) :
         TeensyMenuItem (menu, nullptr, 16, nullptr, nullptr, nullptr, nullptr),
-        _button_bar(*this, 128, 16, 0, 0)
+        _button_bar(*this, 128, 16, 0, 0),
+        _buttonPressedCallback(buttonPressedCallback)
     {
     }
 
@@ -30,6 +53,15 @@ public:
     }
     void ButtonDown(const unsigned char buttonNumber) override {
         _button_bar.ButtonDown(buttonNumber);
+        int selectedButton =_button_bar.GetSelectedIndex();
+        MediaButtonType buttonType;
+        switch (selectedButton) {
+            case 1: buttonType = MediaButtonType::Stop; break;
+            case 2: buttonType = MediaButtonType::Play; break;
+            case 3: buttonType = MediaButtonType::Fastfwd; break;
+            default: buttonType = MediaButtonType::None; break;
+        }
+        _buttonPressedCallback(buttonType);
     }
     void ValueScroll(const bool forward) override {
         _button_bar.IndexScroll(forward);
@@ -44,21 +76,24 @@ public:
 
 protected:
     TeensyMediaButtonBar _button_bar;
+    std::function<void(MediaButtonType)> _buttonPressedCallback;
 
 };
 
 class MediaTimeMenuItem : public TeensyMenuItem {
 public:
-    explicit MediaTimeMenuItem(TeensyMenu &menu) :
-        TeensyMenuItem (menu, nullptr, 12, nullptr, nullptr, nullptr, nullptr),
-        _timeIndicator(*this, 128, 10, 0, 0)
-    {
+    explicit MediaTimeMenuItem(TeensyMenu &menu, MediaPosition &position) : TeensyMenuItem(menu, nullptr, 12, nullptr, nullptr, nullptr,
+                                                                  nullptr), _media_position(position),
+                                                   _timeIndicator(*this, 128, 10, 0, 0) {
         _children.push_back(&_timeIndicator);
     }
 
     ~MediaTimeMenuItem() override = default;
-
+    void SetTime(const unsigned milliseconds) {
+        _timeIndicator.SetTime( _media_position.GetMilliseconds());
+    }
 private:
+    MediaPosition &_media_position;
     TeensyTimeIndicator _timeIndicator;
 };
 
@@ -68,10 +103,14 @@ public:
     MidiSpyScene(View &mainView, int sdChipSelect) : BaseScene(mainView, 128, 128, 0, 0, _bmp_settings_on,
                                                                _bmp_settings_off, 16, 16),
                                                      mainView(mainView),
-                                                     //_button_bar(mainView, 128, 16, 0, 0),
+                                                     _media_position(),
                                                      _menu(mainView, 128, 128, 0, 0, 0x092D /*Sapphire*/, 0),
-                                                     _mediaButtonBarMenuItem(new MediaButtonBarMenuItem(_menu)),
-                                                     _mediaTimeMenuItem(new MediaTimeMenuItem(_menu)),
+                                                     _mediaButtonBarMenuItem(
+                                                         new MediaButtonBarMenuItem(_menu, [&] (MediaButtonType buttonType) {
+                                                             MediaButtonPressed(buttonType);
+                                                         })
+                                                     ),
+                                                     _mediaTimeMenuItem(new MediaTimeMenuItem(_menu, _media_position)),
                                                      sceneMenuItems{
                                                          _mediaButtonBarMenuItem,
                                                          _mediaTimeMenuItem,
@@ -98,15 +137,20 @@ public:
             _menu.Update(milliseconds);
             if (_isRecording) {
                 unsigned long currentMicros = micros();
-                if (_lastMicroseconds == 0)
+                if (_lastMicroseconds == 0) {
                     _lastMicroseconds = currentMicros;
+                    _media_position.SetMilliseconds(0);
+                }
                 else if (currentMicros - _lastMicroseconds > _microsPerTick) {
-                    _currentTicks += (currentMicros - _lastMicroseconds) / _microsPerTick;
+                    auto deltaTicks = currentMicros - _lastMicroseconds;
+                    _currentTicks += deltaTicks / _microsPerTick;
                     _lastMicroseconds = currentMicros;
+                    _media_position.SetMilliseconds(_media_position.GetMilliseconds() + deltaTicks/1000);
                     //Serial.printf("ticks %d", _currentTicks);
                 }
 
                 _microseconds = currentMicros;
+
                 bool writerError = writer.isError();
                 int errorNumber = writerError ? writer.getErrorNumber() : 0;
                 if (writerError != _hasError || errorNumber != _lastErrorNumber) {
@@ -129,6 +173,8 @@ public:
                 }
             }
         }
+        _mediaTimeMenuItem->SetTime(milliseconds);
+        TeensyControl::Update(milliseconds);
     }
 
     void InitScreen () override {
@@ -168,27 +214,33 @@ public:
        _menu.ValueScroll(forward);
     }
     void NoteOn(uint8_t channel, uint8_t pitch, uint8_t velocity) override {
-        auto deltaTicks = _currentTicks - _lastEventTicks;
-        _lastEventTicks = _currentTicks;
-        Serial.print("ON pitch: ");
-        Serial.print(pitch);
-        Serial.print(" velocity: ");
-        Serial.println(velocity);
-        writer.addNoteOnEvent(deltaTicks, channel, pitch, velocity);
+        if (_isRecording) {
+            auto deltaTicks = _currentTicks - _lastEventTicks;
+            _lastEventTicks = _currentTicks;
+            Serial.print("ON pitch: ");
+            Serial.print(pitch);
+            Serial.print(" velocity: ");
+            Serial.println(velocity);
+            writer.addNoteOnEvent(deltaTicks, channel, pitch, velocity);
+        }
     }
     void NoteOff(uint8_t channel, uint8_t pitch, uint8_t velocity) override {
-        auto deltaTicks = _currentTicks - _lastEventTicks;
-        _lastEventTicks = _currentTicks;
-        Serial.print("OFF pitch: ");
-        Serial.print(pitch);
-        Serial.print(" velocity: ");
-        Serial.println(velocity);
-        writer.addNoteOffEvent(deltaTicks, channel, pitch);
+        if (_isRecording) {
+            auto deltaTicks = _currentTicks - _lastEventTicks;
+            _lastEventTicks = _currentTicks;
+            Serial.print("OFF pitch: ");
+            Serial.print(pitch);
+            Serial.print(" velocity: ");
+            Serial.println(velocity);
+            writer.addNoteOffEvent(deltaTicks, channel, pitch);
+        }
     }
     void ControlChange(uint8_t channel, uint8_t data1, uint8_t data2) override {
-        auto deltaTicks = _currentTicks - _lastEventTicks;
-        _lastEventTicks = _currentTicks;
-        writer.addControlChange(deltaTicks, data1, data2, channel);
+        if (_isRecording) {
+            auto deltaTicks = _currentTicks - _lastEventTicks;
+            _lastEventTicks = _currentTicks;
+            writer.addControlChange(deltaTicks, data1, data2, channel);
+        }
     }
 
     unsigned int get_microseconds_per_tick(double beats_per_minute) {
@@ -210,6 +262,7 @@ public:
         _lastMicroseconds = 0;
         _currentTicks = 0;
         _lastEventTicks = 0;
+        _menu.ForceRedraw();
     }
 
     void StopRecording() {
@@ -218,11 +271,25 @@ public:
         writer.addEndofTrack(0,0);
         writer.close();
         _isRecording = false;
+        _menu.ForceRedraw();
     }
 
+    void MediaButtonPressed(const MediaButtonType buttonType) {
+        switch (buttonType) {
+            case MediaButtonType::None: break;
+            default: {
+                if (!_isRecording)
+                    StartRecording();
+                else
+                    StopRecording();
+                break;
+            };
+        }
+    }
 private:
     View &mainView;
     TeensyMenu _menu;
+    MediaPosition _media_position;
     MediaButtonBarMenuItem *_mediaButtonBarMenuItem;
     MediaTimeMenuItem *_mediaTimeMenuItem;
     TeensyMenuItem *sceneMenuItems[3];
